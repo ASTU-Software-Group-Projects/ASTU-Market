d09ea38d506dfb70ce8e8468b7fb53df83ff020a
Matiwos Kebede <matiwoskebede01@gmail.com>
2026-05-02 12:25:29 +0300

    Add OrderStatus enum with companion object

--- app/src/main/java/com/market/astu/data/model/OrderStatus.kt
+++ app/src/main/java/com/market/astu/data/model/OrderStatus.kt
@@ -1,2 +1,17 @@
 package com.market.astu.data.model
 
+enum class OrderStatus {
+    AWAITING_RUNNER,
+    RUNNER_ASSIGNED,
+    PICKED_UP,
+    IN_TRANSIT,
+    DELIVERED,
+    DISPUTED,
+    CANCELLED;
+
+    companion object {
+        fun fromValue(value: String?): OrderStatus {
+            return entries.firstOrNull { it.name == value } ?: AWAITING_RUNNER
+        }
+    }
+}
