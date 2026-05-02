e5f2bc587aa065e51ab2524a23b84402459b9307
Matiwos Kebede <matiwoskebede01@gmail.com>
2026-05-02 12:32:47 +0300

    Add Wallet data class with Firebase Timestamp fields

--- app/src/main/java/com/market/astu/data/model/Wallet.kt
+++ app/src/main/java/com/market/astu/data/model/Wallet.kt
@@ -1,2 +1,11 @@
 package com.market.astu.data.model
 
+import com.google.firebase.Timestamp
+
+data class Wallet(
+    val uid: String = "",
+    val balance: Double = 0.0,
+    val monthlyYieldEarned: Double = 0.0,
+    val lastYieldDate: Timestamp? = null,
+    val updatedAt: Timestamp? = null
+)
