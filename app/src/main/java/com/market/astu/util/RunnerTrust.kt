17e8dddd5fc028f06d5040df5cdc57a22218b816
Hilme.Y <yosefayalew46@gmail.com>
2026-05-07 15:43:59 +0300

    Add RunnerTrustBand data class and scoring function

--- app/src/main/java/com/market/astu/util/RunnerTrust.kt
+++ app/src/main/java/com/market/astu/util/RunnerTrust.kt
@@ -1,1 +1,15 @@
 package com.market.astu.util
+
+data class RunnerTrustBand(
+    val label: String,
+    val maxOrderValue: Double
+)
+
+fun runnerTrustBand(score: Int): RunnerTrustBand {
+    return when (score.coerceIn(0, 100)) {
+        in 0..19 -> RunnerTrustBand("Bronze", 20.0)
+        in 20..49 -> RunnerTrustBand("Silver", 100.0)
+        in 50..79 -> RunnerTrustBand("Gold", 500.0)
+        else -> RunnerTrustBand("Platinum", 2000.0)
+    }
+}
